8f55037f82d56b5b6e025d3269f350ffe07ae90c
Ashley Gorringe <ashley@gorringe.email>
2024-04-24 15:15:18 +0100

    Development

--- src/components/ModalViewItem.js
+++ src/components/ModalViewItem.js
@@ -7,7 +7,9 @@ function ModalViewItem(props){
 
     return(
         <div className='modal-body'>
-            <p>Item details</p>
+            <span className='item-price'>{props.modalData['item']['price']}</span>
+            <div className='item-image' style={{ backgroundImage: `url(${props.modalData['item']['image']})` }}></div>
+            <p className='item-notes'>{props.modalData['item']['notes']}</p>
         </div>
     );
 }
